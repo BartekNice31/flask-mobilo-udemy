b3339e31b7f082d169e19a75836728c640374e0b
BartekNice31 <bartlomiej.nicewicz1993@gmail.com>
2026-06-04 19:29:21 +0200

    Pierwszy commit

--- nicerapp/exercise/app.py
+++ nicerapp/exercise/app.py
@@ -1,13 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,flash
 from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv
+load_dotenv(r"C:\Users\Bartłomiej\Desktop\python\flask-mobilo-udemy-main\.flaskenv")
 
-load_dotenv(r".flaskenv") 
 app=Flask(__name__)
 bootstrap=Bootstrap(app=app)
 
 @app.route("/")
-def index():
+def route():
     return render_template("index.html")
 
 if __name__=="__main__":
